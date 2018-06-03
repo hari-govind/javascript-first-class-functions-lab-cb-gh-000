83f9c5ed006b63691ad980e81712403a11078d4e
Hari G <hgs@disroot.org>
2018-06-03 13:51:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,3 @@ function countdown(callback){
   },2000);
 }
 
-function multiplierValue(num){
-  var doubler = function() {
-    return 2*num;
-  }();
-  var tripler = function(){
-    return 3*num;
-  }();
-}
